5723ed60a7c7b0c422f48c0a0204dbd93397fb90
ireshdilsh <ireshdilshan46@gmail.com>
2024-12-07 11:39:55 +0530

    update login page design

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,4 +1,4 @@
-import { TouchableOpacity,View, Text, ImageBackground } from "react-native";
+import { TouchableOpacity,View, Text, ImageBackground, Pressable } from "react-native";
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 import React from "react";
@@ -11,6 +11,10 @@ const Login = () => {
 
   const continueWithFacebook = () => {
     console.log("Continue with Facebook Button Clicked")
+  }
+
+  const gotoRegisterPage = () => {
+    console.log("Go to Register");
   }
 
   return (
@@ -70,12 +74,18 @@ const Login = () => {
       </Text>
 
       {/* continu with email button and continue with google button*/}
-      <View style = {{marginTop:40,display:'flex',justifyContent:'center',alignItems:'center',width:'100%'}}>
+      <View style = {{marginTop:50,display:'flex',justifyContent:'center',alignItems:'center',width:'100%'}}>
       <TouchableOpacity onPress={continueWithGoogle} style = {{backgroundColor:'#fff',height:50,width:'85%',borderWidth:1,borderColor:'#000',display:'flex',justifyContent:'center',alignItems:'center',borderRadius:8}}><Text style = {{fontWeight:700,color:'#000',fontFamily:'Gabarito'}}>     Continue with Google     <FontAwesome name="google" size={15} color="black"/></Text></TouchableOpacity>
       </View>
       <View style = {{marginTop:10,display:'flex',justifyContent:'center',alignItems:'center',width:'100%'}}>
       <TouchableOpacity onPress={continueWithFacebook} style = {{backgroundColor:'#2563eb',height:50,width:'85%',display:'flex',justifyContent:'center',alignItems:'center',borderRadius:8}}><Text style = {{color:'#fff',fontFamily:'Gabarito',fontWeight:700}}>     Continue with Facebook     <FontAwesome5 name="facebook" size={17} color="#fff"/></Text></TouchableOpacity>
       </View>
+
+        <View style = {{marginTop:80,display:'flex',justifyContent:'space-between',alignItems:'center'}}>
+        <Text style = {{fontFamily:'Gabarito',fontSize:15,color:'#9ca3af'}}>If you haven't an account ?</Text>
+        <Pressable style = {{display:'flex',justifyContent:'center',alignItems:'center'}} onPress={gotoRegisterPage}><Text style = {{fontSize:15,fontFamily:'Gabarito',textAlign:'center',color:'#1d4ed8'}}> Create Account</Text></Pressable>
+        </View>
+
     </View>
   );
 };
